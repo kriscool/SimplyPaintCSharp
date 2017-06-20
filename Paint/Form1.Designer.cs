c5c6447717a23d3b23dbfb06b2475eac89e536a7
Krzysztof Karalus <krzysztof.karalus95@gmail.com>
2017-06-20 21:51:54 +0200

    App

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button minus;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.plus = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.GreyScale = new System.Windows.Forms.Button();
+            this.SecondScale = new System.Windows.Forms.Button();
+            this.ThirdScale = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.SaveFile = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            minus = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // minus
+            // 
+            minus.Location = new System.Drawing.Point(3, 59);
+            minus.Name = "minus";
+            minus.Size = new System.Drawing.Size(56, 23);
+            minus.TabIndex = 1;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = true;
+            minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1334, 480);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1335, 481);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.GreyScale, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.SecondScale, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.ThirdScale, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Save, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.SaveFile, 1, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1071, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(261, 475);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Choose Image";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(minus, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.plus, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 121);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(124, 112);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Size";
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(65, 59);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(56, 23);
+            this.plus.TabIndex = 2;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.button3, 0, 0);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(133, 121);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(125, 112);
+            this.tableLayoutPanel4.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ChooseColor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // GreyScale
+            // 
+            this.GreyScale.Location = new System.Drawing.Point(3, 3);
+            this.GreyScale.Name = "GreyScale";
+            this.GreyScale.Size = new System.Drawing.Size(124, 46);
+            this.GreyScale.TabIndex = 5;
+            this.GreyScale.Text = "GreyScale";
+            this.GreyScale.UseVisualStyleBackColor = true;
+            this.GreyScale.Click += new System.EventHandler(this.GreyScale_Click);
+            // 
+            // SecondScale
+            // 
+            this.SecondScale.Location = new System.Drawing.Point(3, 239);
+            this.SecondScale.Name = "SecondScale";
+            this.SecondScale.Size = new System.Drawing.Size(124, 45);
+            this.SecondScale.TabIndex = 6;
+            this.SecondScale.Text = "SecondScale";
+            this.SecondScale.UseVisualStyleBackColor = true;
+            this.SecondScale.Click += new System.EventHandler(this.SecondScale_Click);
+            // 
+            // ThirdScale
+            // 
+            this.ThirdScale.Location = new System.Drawing.Point(133, 239);
+            this.ThirdScale.Name = "ThirdScale";
+            this.ThirdScale.Size = new System.Drawing.Size(125, 45);
+            this.ThirdScale.TabIndex = 7;
+            this.ThirdScale.Text = "ThirdScale";
+            this.ThirdScale.UseVisualStyleBackColor = true;
+            this.ThirdScale.Click += new System.EventHandler(this.ThirdScale_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(3, 357);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(124, 45);
+            this.Save.TabIndex = 8;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Location = new System.Drawing.Point(133, 357);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(125, 22);
+            this.SaveFile.TabIndex = 9;
+            this.SaveFile.TextChanged += new System.EventHandler(this.SaveFile_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1062, 475);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 505);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button GreyScale;
+        private System.Windows.Forms.Button SecondScale;
+        private System.Windows.Forms.Button ThirdScale;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox SaveFile;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using System.Drawing.Drawing2D;
+using AForge;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        String File;
+        SolidBrush color;
+        Bitmap DrawArea;
+        Graphics g;
+        Pen pen;
+        Bitmap image;
+        Color c;
+        int size1 = 10;
+        int size2 = 10;
+        SolidBrush myBrush;
+        public Form1()
+        {
+            InitializeComponent();
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            g = Graphics.FromImage(DrawArea);
+            myBrush = new SolidBrush(Color.Black);
+            c = Color.Black;
+            pen = new Pen(c, 20);
+            textBox2.Text = size1.ToString();
+            
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                g.FillEllipse(pen.Brush, e.X, e.Y, size1, size2);
+                pictureBox1.Image = DrawArea;
+            
+            }
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            File = openFileDialog1.FileName;
+                            image = new Bitmap(@File);
+
+
+
+                            g.DrawImage(image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                            pictureBox1.Image = DrawArea;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            size1--;
+            size2--;
+
+            textBox2.Text = size1.ToString();
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            size1++;
+            size2++;
+            textBox2.Text = size1.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+           
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                c = MyDialog.Color;
+            pen = new Pen(c);
+        }
+
+        private void GreyScale_Click(object sender, EventArgs e)
+        {
+           
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            DrawArea = filter.Apply(DrawArea);
+            pictureBox1.Image = DrawArea;
+
+            makeScreenCopy();
+        }
+
+        private void makeScreenCopy()
+        {
+            Image screenCopy = pictureBox1.Image;
+            DrawArea = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(DrawArea);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            try
+            {
+                g.DrawImage(screenCopy, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            }catch
+            {
+
+            }
+            pictureBox1.Image = DrawArea;
+        }
+
+        private void SecondScale_Click(object sender, EventArgs e)
+        {
+            LevelsLinear filter = new LevelsLinear();
+            filter.InRed = new IntRange(30, 320);
+            filter.InGreen = new IntRange(50, 240);
+            filter.InBlue = new IntRange(10, 210);
+            DrawArea = filter.Apply(DrawArea);
+            pictureBox1.Image = DrawArea;
+            
+            makeScreenCopy();
+        }
+
+        private void ThirdScale_Click(object sender, EventArgs e)
+        {
+            Edges filter = new Edges();
+            DrawArea = filter.Apply(DrawArea);
+            pictureBox1.Image = DrawArea;
+
+            makeScreenCopy();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+
+            savefile.FileName = SaveFile.Text;
+            if (savefile.FileName != null)
+            {
+                savefile.Filter = "Files|*.jpg;*.jpeg;";
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    image.Dispose();
+
+                    if (System.IO.File.Exists(savefile.FileName))
+                        System.IO.File.Delete(savefile.FileName);
+
+                    DrawArea.Save(savefile.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private void SaveFile_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            makeScreenCopy();
+        }
+    }
+}
+
